5fc8b82fd938e5030fa9a2b2dc0c48bdcd940ccc
yongruifang <1102649231@qq.com>
2023-06-15 19:42:40 +0800

    feat: 得到上三角矩阵

--- gauss.c
+++ gauss.c
@@ -55,25 +55,75 @@ int main(int argc, char* argv[]) {
         rmap[i] = -1;
     }
     double local_pivot_val, global_pivot_val;
-    int local_pivot_idx, global_pivot_idx;
+    int local_pivot_row, global_pivot_row;
     for (i = 0; i < N; i ++) {// 逐列消元
         local_pivot_val = -1e10;
-        local_pivot_idx = -1;
+        local_pivot_row = -1;
         // Find the local pivot row with the maximum absolute value
         for(int row = 0; row < N/numproc; row++){ //逐行找主元
             int gRow = row*numproc + rank; // gRow是全局行号 
             if(rmap[gRow] < 0){ //第J行的主元为空
-                if(M_buffer[row*COL+i] > local_pivot_val){
+                if(M_buffer[row*COL+i] > local_pivot_val){ 
                     local_pivot_val = M_buffer[row*COL+i];
-                    local_pivot_idx = row;
+                    local_pivot_row = row;
                 }                
             }
         }
+        // Find the global pivot row with the maximum absolute value
         MPI_Allreduce(&local_pivot_val,&global_pivot_val,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
-        if(fabs(global_pivot_val-local_pivot_val)<0.0001){
-            printf("rank %d, global_pivot_val = %f\n",rank,global_pivot_val);
+        int local_pivot_proc = -1;// 记录主元所在的进程号
+        double *temp_row = (double*)malloc(sizeof(double)*COL); // 临时存储行
+        if(fabs(global_pivot_val-local_pivot_val)<0.0001){ 
+            local_pivot_proc = rank; 
+            for(j = 0 ; j < N+1; j ++){
+                temp_row[j] = M_buffer[local_pivot_row*COL+j]; // 保存该行
+            }
+            global_pivot_row = local_pivot_row * numproc + rank; // 该行在全局矩阵中的位置
         }
-    }    
+        int global_pivot_proc; // 因为有可能有多个进程的主元值相同，所以需要再次通信
+        MPI_Allreduce(&local_pivot_proc, &global_pivot_proc, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD); // 取最大的进程号
+        MPI_Bcast(&global_pivot_proc,1,MPI_INT,0,MPI_COMM_WORLD); // 广播主元所在的进程
+        MPI_Bcast(temp_row,COL,MPI_DOUBLE,global_pivot_proc,MPI_COMM_WORLD); // 广播主元所在的行
+        MPI_Bcast(&global_pivot_row,1,MPI_INT,global_pivot_proc,MPI_COMM_WORLD); // 广播主元所在的行号
+        cmap[i] = global_pivot_row; // 第i列的主元在第global_pivot_row行
+        rmap[global_pivot_row] = i; // 第global_pivot_row行的主元在第i列
+        for(int row = 0; row < N/numproc; row ++){
+            if(rmap[row*numproc+rank]<0){ // 第row行的主元为空，需要消元
+                double temp = M_buffer[row*COL+i] / temp_row[i]; // 计算消元系数
+                for(j = i; j < N+1; j ++){ // 逐列消元
+                    M_buffer[row*COL+j] -= temp_row[j]*temp; // 消元
+                }
+            }
+        }
+    }
+    // 收集矩阵
+    MPI_Gather(M_buffer,N*COL/numproc,MPI_DOUBLE,M,N*COL/numproc,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    // 打印上三角矩阵
+    if(rank == 0){
+        // 记录最后一行的映射
+        for(i = 0 ; i < N; i ++){
+            if(rmap[i] == -1){ 
+                cmap[N-1] = i; // 记录最后一行的映射
+            }
+        }        
+        for(i = 0 ; i < N; i ++){
+            printf("%d ",cmap[i]);
+        }
+        printf("\n");
+        for(i = 0 ; i < N; i ++){
+            printf("%d ",rmap[i]);
+        }
+        printf("\n");
+        printf("Upper triangular matrix:\n");
+        for (i = 0; i < N; i++) {
+            int row = cmap[i]; 
+            int index = row % numproc*(N/numproc) + row / numproc; // 该行在全局矩阵中的位置
+            for (j = 0; j < N; j ++) {
+                printf("%f ", M[index*COL+j]);
+            }
+            printf("\n");
+        }
+    }
     if(rank==0){
         // 释放内存
         free(M);
