ca4a656e70d7dfb78252d8181f7699cb97226a08
yongruifang <1102649231@qq.com>
2023-06-15 17:18:37 +0800

    feat: 寻找主元

--- gauss.c
+++ gauss.c
@@ -44,20 +44,42 @@ int main(int argc, char* argv[]) {
     int bsize = N*COL/numproc;
     M_buffer = (double*)malloc(sizeof(double)*bsize);
     MPI_Scatter(M_remake,N*COL/numproc,MPI_DOUBLE,M_buffer,bsize,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    // 打印局部矩阵
-    printf("rank = %d\n",rank);
-    for(i=0;i<bsize/COL;i++){
-        for(j=0;j<COL;j++){
-            printf("%lf ",M_buffer[i*COL+j]);
-        }
-        printf("\n");
+    // Perform Gaussian elimination with partial pivoting
+    // 引入双向映射，另类实现行交换
+    int *cmap = (int*)malloc(N*sizeof(int)); //cmap[i]=j 表示第i列的主元在第j行
+    int *rmap = (int*)malloc(N*sizeof(int)); //rmap[i]=j 表示第i行的主元在第j列
+    for (i = 0; i < N; i ++){
+        cmap[i] = -1;
+        rmap[i] = -1;
     }
+    double local_pivot_val, global_pivot_val;
+    int local_pivot_idx, global_pivot_idx;
+    for (i = 0; i < N; i ++) {// 逐列消元
+        local_pivot_val = -1e10;
+        local_pivot_idx = -1;
+        // Find the local pivot row with the maximum absolute value
+        for(int row = 0; row < N/numproc; row++){ //逐行找主元
+            int gRow = row*numproc + rank; // gRow是全局行号 
+            if(rmap[gRow] < 0){ //第J行的主元为空
+                if(fabs(M_buffer[row*COL+i]) > local_pivot_val){
+                    local_pivot_val = fabs(M_buffer[row*COL+i]);
+                    local_pivot_idx = row;
+                }                
+            }
+        }
+        MPI_Allreduce(&local_pivot_val,&global_pivot_val,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+        if(fabs(global_pivot_val-local_pivot_val)<0.0001){
+            printf("rank %d, global_pivot_val = %f\n",rank,global_pivot_val);
+        }
+    }    
     if(rank==0){
         // 释放内存
         free(M);
         free(M_remake) ;
         free(M_buffer);
         free(x);
+        free(cmap);
+        free(rmap);
     }
     MPI_Finalize();
     return 0;
